3b365b063d754acfe99f2307c43f7294467e7cd3
JoaoEnrique <joao.alves254@etec.sp.gov.br>
2022-06-08 14:33:14 -0300

    Primeiro Fragment

---
+++ app/src/main/java/com/example/appnaivigation/first_fragment.kt
@@ -0,0 +1,22 @@
+package com.example.appnaivigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class first_fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        view.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_first_fragment_to_second_fragment)}
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/appnaivigation/second_fragment.kt
@@ -0,0 +1,23 @@
+package com.example.appnaivigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class second_fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
+        view.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_second_fragment_to_first_fragment)}
+
+        return view
+    }
+}
